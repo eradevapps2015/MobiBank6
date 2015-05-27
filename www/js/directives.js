3df44fbb854b33f72b2edd61e21b334356d1060c
eradevapps2015 <eradevapps2015@users.noreply.github.com>
2015-05-27 17:02:00 +0600

    Update directives.js

--- www/js/directives.js
+++ www/js/directives.js
@@ -1,7 +1,12 @@
 angular.module('starter.directives', [])
 
-.directive('map', function() {
-var myCenter=new google.maps.LatLng(23.730182 , 90.408583);
+
+.directive('map', function($rootScope) {
+
+
+//alert($rootScope.branchMapLongitude);
+
+var myCenter=new google.maps.LatLng( $rootScope.branchMapLatitude , $rootScope.branchMapLongitude);
   return {
     restrict: 'E',
     scope: {
@@ -9,50 +14,39 @@ var myCenter=new google.maps.LatLng(23.730182 , 90.408583);
     },
     link: function ($scope, $element, $attr) {
       function initialize() {
+	 // $scope.$apply();
         var mapOptions = {
           center:myCenter,
           zoom:16,
           mapTypeId: google.maps.MapTypeId.ROADMAP
         };	
 		
-       //var map = new google.maps.Map($element[0], mapOptions);
-		
-		var map = new google.maps.Map($element[0],  mapOptions);
-		
-		
+       //var map = new google.maps.Map($element[0], mapOptions);		
+		var map = new google.maps.Map($element[0],  mapOptions);		
 		 var marker = new google.maps.Marker({
       position: myCenter,
       map: map,
-      title: 'ERA InfoTech Limited'
+      title:   $rootScope.branchMapBranchName
     });
-
-   
-		
-		
-     
-	 
-		
-		
-
+	
+  //$scope.mm=map;
         // Stop the side bar from dragging when mousedown/tapdown on the map
         google.maps.event.addDomListener(marker,$element[0], 'mousedown', function (e) {
           e.preventDefault();
           return false;
-        });
-		
-		
-		   $scope.onCreate({map: map});
-
-		
+        });			
+		   $scope.onCreate({map: map});		
       }
-
       if (document.readyState === "complete") {
         initialize();
-      } else {
-        google.maps.event.addDomListener(window, 'load', initialize);
-      }
-	  
-	
+      } else {	 
+	 
+        //google.maps.event.addDomListener(window, 'load', initialize);	
+		google.maps.event.addDomListener(window, 'load', initialize);
+		initialize();		
+      }	
     }
   }
+
+
 });
